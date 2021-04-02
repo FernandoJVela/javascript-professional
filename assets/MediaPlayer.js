93779025f480c98ba2f44c94db0119e81462c12c
Fernando Vela <fer.velav@gmail.com>
2021-04-01 20:58:34 -0500

    Started media player plugins development for Platzi project. Play button not working.

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,46 @@
+function MediaPlayer(config){
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function(){
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+}
+
+MediaPlayer.prototype.play = function(){
+let playPromise = this.media.play();
+  if (playPromise !== undefined) {
+    playPromise.then(_ => {
+      // Automatic playback started!
+      // Show playing UI.
+    })
+    .catch(error => {
+      // Auto-play was prevented
+      // Show paused UI.
+    });
+  }
+}
+MediaPlayer.prototype.pause = function(){
+    this.media.pause();
+}
+
+MediaPlayer.prototype.togglePlay = function(){
+    if(this.media.paused){
+        this.media.play();
+    }else{
+        this.media.pause();
+    }
+}
+
+MediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function(){
+    this.media.muted = false;
+}
+
+export default MediaPlayer;
